ed5ffe16d7946b5ad5dcec26f548a6258ae8f178
n1-NISHANT <nk98nishant@gmail.com>
2025-01-13 21:33:56 +0530

    Update PositiveNegative.java

--- Basic/Numbers/PositiveNegative.java
+++ Basic/Numbers/PositiveNegative.java
@@ -1,1 +1,17 @@
+package DSA.Basic.Numbers;
+import java.util.Scanner;
 
+public class PositiveNegative{
+    public static void main(String[]args){
+           Scanner sc = new Scanner(System.in);
+           int value = sc.nextInt();
+           if(value == 0){
+              System.out.print("Given Number is "+value);
+           } else if(value > 0){
+                    System.out.print("Given Number is Positive "+value);
+           }else if(value < 0){
+                      System.out.print("Given Number is Negative "+value);
+           }  
+         sc.close();
+    }
+}
