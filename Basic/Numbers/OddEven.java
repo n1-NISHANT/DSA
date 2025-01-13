89d24928af1b319a9657496015e0a7bf7b6653cc
n1-NISHANT <nk98nishant@gmail.com>
2025-01-13 22:22:32 +0530

    Update OddEven.java
    Using if else statement

--- Basic/Numbers/OddEven.java
+++ Basic/Numbers/OddEven.java
@@ -1,1 +1,16 @@
+package DSA.Basic.Numbers;
+import java.util.Scanner;
 
+public class OddEven{
+    public static void main(String[]args){
+           Scanner sc = new Scanner(System.in);
+           int value = sc.nextInt();
+
+           if(value % 2 == 0){
+                  System.out.println("Given value is even ");
+           }else{
+                 System.out.println("Given number is Odd");
+           } 
+      sc.close();
+    }
+}
