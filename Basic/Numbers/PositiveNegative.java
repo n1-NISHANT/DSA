defddba727c443f762d55077dd33cabb11df5a38
n1-NISHANT <nk98nishant@gmail.com>
2025-01-13 21:55:08 +0530

    Update PositiveNegative.java
    Solv problem using Ternary Operator

--- Basic/Numbers/PositiveNegative.java
+++ Basic/Numbers/PositiveNegative.java
@@ -5,13 +5,18 @@ public class PositiveNegative{
     public static void main(String[]args){
            Scanner sc = new Scanner(System.in);
            int value = sc.nextInt();
-           if(value == 0){
+        /*   Using if else Statement
+             if(value == 0){
               System.out.print("Given Number is "+value);
            } else if(value > 0){
                     System.out.print("Given Number is Positive "+value);
            }else if(value < 0){
                       System.out.print("Given Number is Negative "+value);
            }  
+         */
+
+            String stringvalue = (value==0?"Given Number is "+value:(value>0)?"Given Number is Positive "+value:"Given Number is Negative "+value);
+	        System.out.println(stringvalue);
          sc.close();
     }
 }
