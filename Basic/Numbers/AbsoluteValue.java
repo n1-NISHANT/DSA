bd5f9c63f9581b188fc1241a6757b9841d075461
n1-NISHANT <nk98nishant@gmail.com>
2025-01-13 22:09:42 +0530

    Update AbsoluteValue.java
    Using if else condition

--- Basic/Numbers/AbsoluteValue.java
+++ Basic/Numbers/AbsoluteValue.java
@@ -1,1 +1,16 @@
+package DSA.Basic.Numbers;
+import java.util.Scanner;
 
+public class AbsoluteValue{
+  // Absolute value is value between 0 and value. ex--> value is -5(difference between 0 to -5 is 5) so absolute value is 5
+    public static void main(String[]args){
+           Scanner sc = new Scanner(System.in);
+           int value = sc.nextInt();
+                if( value >= 0){
+                    System.out.println(value);
+                } else {
+                   System.out.println(-(value));
+                }
+          sc.close();
+    }
+}
