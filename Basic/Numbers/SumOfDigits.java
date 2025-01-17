58a3effe5a79ff2577bae268518d7348d5d41d00
n1-NISHANT <nk98nishant@gmail.com>
2025-01-17 23:25:55 +0530

    Update SumOfDigits.java

--- Basic/Numbers/SumOfDigits.java
+++ Basic/Numbers/SumOfDigits.java
@@ -1,1 +1,16 @@
+package DSA.Basic.Numbers;
+import java.util.Scanner;
 
+public class SumOfDigits{
+public static void main(String[] args) {
+    int num;
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+     int sum = 0;
+    while(num>0){
+        sum += num%10;
+        num /=10;
+    }
+    System.out.println(sum);
+}
+}
