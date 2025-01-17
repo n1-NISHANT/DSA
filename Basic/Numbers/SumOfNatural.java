ed0c331e254b37fc2d9b7cc43d8ca52e8eb817d2
n1-NISHANT <nk98nishant@gmail.com>
2025-01-17 23:09:24 +0530

    Update SumOfNatural.java

--- Basic/Numbers/SumOfNatural.java
+++ Basic/Numbers/SumOfNatural.java
@@ -1,1 +1,15 @@
+package DSA.Basic.Numbers;
+import java.util.Scanner;
+
+public class SumOfNatural{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int Num = sc.nextInt();
+    int sum = 0;
+    for(int i = 1 ; i<=Num ;i++){
+        sum +=i;
+    }
+    System.out.println(sum);
+}
+}
 
