1ca515175aca4b5c47107661303d15797f2a70c3
n1-NISHANT <nk98nishant@gmail.com>
2025-01-17 23:28:08 +0530

    Update Reverse.java

--- Basic/Numbers/Reverse.java
+++ Basic/Numbers/Reverse.java
@@ -1,1 +1,16 @@
+package DSA.Basic.Numbers;
+import java.utiil.Scanner;
 
+public class Reverse{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int ans = 0;
+    while(num>0){
+        int mod = num%10;
+        ans = ans*10 + mod;
+        num /=10;
+    }
+    System.out.println(ans);
+}
+}
