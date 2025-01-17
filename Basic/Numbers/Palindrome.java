bc05f9b790c6ecc8c86d3f409955546a7443b027
n1-NISHANT <nk98nishant@gmail.com>
2025-01-17 23:32:42 +0530

    Update Palindrome.java

--- Basic/Numbers/Palindrome.java
+++ Basic/Numbers/Palindrome.java
@@ -1,1 +1,22 @@
+package DSA.Basic.Number;
+import java.util.Scanner;
 
+public class Palindrome{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    
+    int rev = 0;
+    int check = num;
+    while(check>0){
+        int mod = check%10;
+        rev = rev*10+mod;
+        check /= 10;
+    }
+        if(num == rev){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
+}
+}
