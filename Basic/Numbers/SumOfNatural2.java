690d9a53eee6055036e5d55066032ebb88e698bc
n1-NISHANT <nk98nishant@gmail.com>
2025-01-17 23:16:17 +0530

    Update SumOfNatural2.java

--- Basic/Numbers/SumOfNatural2.java
+++ Basic/Numbers/SumOfNatural2.java
@@ -1,1 +1,11 @@
+package DSA.Basic.Number;
+import java.util.Scanner;
+
+public class SumOfNatural2{
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N*(N+1)/2);
+  }
+}
 
