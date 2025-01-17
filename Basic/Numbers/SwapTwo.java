8aa2bd0df9c25766b7183343ed8f8557d65378bd
n1-NISHANT <nk98nishant@gmail.com>
2025-01-17 23:05:24 +0530

    Update SwapTwo.java

--- Basic/Numbers/SwapTwo.java
+++ Basic/Numbers/SwapTwo.java
@@ -1,1 +1,15 @@
+package DSA.Basic.Numbers;
+import java.util.Scanner;
 
+public class SwapTwo{
+    public static void main(String[] args) {
+    int a, b , c;
+    Scanner sc = new Scanner(System.in);
+    a = sc.nextInt();
+    b = sc.nextInt();
+    c=a;
+    a=b;
+    b=c;
+    System.out.println(""+a+b);
+}
+}
