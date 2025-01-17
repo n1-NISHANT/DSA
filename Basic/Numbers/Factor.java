355a9ae5e2886eeaba8df0b797e4dc7c786bee43
n1-NISHANT <nk98nishant@gmail.com>
2025-01-17 23:19:08 +0530

    Update Factor.java

--- Basic/Numbers/Factor.java
+++ Basic/Numbers/Factor.java
@@ -1,1 +1,17 @@
+package DSA.Basic.Numbers;
+import java.util.Scanner;
+
+public class Factor{
+public static void main(String[] args) {
+    int N, X;
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+    X = sc.nextInt();
+    if(N%X==0){
+        System.out.println("Yes");
+    }else{
+        System.out.println("No");
+    }
+}
+}
 
