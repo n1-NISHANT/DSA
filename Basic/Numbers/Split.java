585f580811a65b836addc5bb173aa2b3db036586
n1-NISHANT <nk98nishant@gmail.com>
2025-01-17 23:22:48 +0530

    Update Split.java

--- Basic/Numbers/Split.java
+++ Basic/Numbers/Split.java
@@ -1,1 +1,14 @@
+package DSA.Basic.Numbers;
+import java.util.Scanner;
 
+public class Split{
+public static void main(String[] args) {
+    int num;
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    while(num>0){
+       System.out.println(num%10);
+       num /=10;
+    }
+}
+}
