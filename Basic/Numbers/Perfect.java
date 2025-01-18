4191454865687674e7c578cbc6b91ae5a347b209
n1-NISHANT <nk98nishant@gmail.com>
2025-01-18 21:52:52 +0530

    Update Perfect.java

--- Basic/Numbers/Perfect.java
+++ Basic/Numbers/Perfect.java
@@ -2,6 +2,20 @@ package DSA.Basic.Numbers;
 
 import java.util.Scanner;
 public class Perfect{
-
-
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int sum=1; 
+   
+    for(int i = 2;i<n;i++){
+        if(n%i==0){
+            sum += i;
+        }
+    }
+    if(n == sum){
+        System.out.println("Yes");
+    }else{
+        System.out.println("No");
+    }
+}
 }
