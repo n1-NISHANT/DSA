5e9ef0341dc3782337769eabbae00f10f89142a1
n1-NISHANT <nk98nishant@gmail.com>
2025-01-18 21:44:40 +0530

    Update Armstrong.java

--- Basic/Numbers/Armstrong.java
+++ Basic/Numbers/Armstrong.java
@@ -1,1 +1,21 @@
+package DSA.Basic.Numbers;
 
+import java.util.Scanner;
+public class Armstrong{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int temp = num;
+    int ans = 0;
+    while(num>0){
+        int mod = num%10;
+        ans += mod * mod *mod;
+        num /= 10;
+    }
+    if(ans == temp){
+        System.out.println("Armstrong number");
+    }else{
+        System.out.println("Not Armstrong number");
+    }
+}
+}
