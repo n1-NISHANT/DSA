f4531560e85e5f9ab88da085542f45a2ae0aeba6
n1-NISHANT <nk98nishant@gmail.com>
2025-01-18 21:50:27 +0530

    Update Prime.java

--- Basic/Numbers/Prime.java
+++ Basic/Numbers/Prime.java
@@ -2,6 +2,19 @@ package DSA.Basic.Numbers;
 
 import java.util.Scanner;
 public class Prime{
-
-
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int  num = sc.nextInt();
+    int flag = 0;
+    for(int i = 2;i<num;i++){
+        if(num%i==0){
+            flag = 1;
+        }
+    }
+    if(flag == 0){
+        System.out.println(" Prime number");
+    }else{
+         System.out.println("Not Prime number");
+    }
+}
 }
