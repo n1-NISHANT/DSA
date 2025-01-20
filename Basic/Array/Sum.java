03661f9180e518a64566057bd322c7e880913cc3
n1-NISHANT <nk98nishant@gmail.com>
2025-01-20 21:44:36 +0530

    Update Sum.java

--- Basic/Array/Sum.java
+++ Basic/Array/Sum.java
@@ -2,6 +2,18 @@ package DSA.Basic.Array;
 
 import java.util.Scanner;
 public class Sum{
-
-
+public static void main(String[] args) {
+    int[] arr = new int[10];
+    int n, i;
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    
+    for(i = 0; i < n; i++)
+    arr[i] = sc.nextInt();
+    int sum = 0;
+    for(i=0;i<n;i++){
+        sum += arr[i]; 
+    }
+    System.out.println(sum);
+}
 }
