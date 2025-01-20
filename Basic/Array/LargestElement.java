793f970dd336b3cc52cc1d261e03f9806c1ee044
n1-NISHANT <nk98nishant@gmail.com>
2025-01-20 21:45:50 +0530

    Update LargestElement.java

--- Basic/Array/LargestElement.java
+++ Basic/Array/LargestElement.java
@@ -2,6 +2,19 @@ package DSA.Basic.Array;
 
 import java.util.Scanner;
 public class LargestElement{
-
-
+public static void main(String[] args) {
+    int[] arr = new int[10];
+    int n, i;
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    for (i = 0; i < n; i++)
+        arr[i] = sc.nextInt();
+    int max =0;
+    for(i=0;i<n;i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+    }
+    System.out.println("Max ="+max);
+}
 }
