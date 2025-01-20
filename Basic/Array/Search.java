25cdd3805a466226cd74dc755f99cce5b24feb9e
n1-NISHANT <nk98nishant@gmail.com>
2025-01-20 21:50:20 +0530

    Update Search.java

--- Basic/Array/Search.java
+++ Basic/Array/Search.java
@@ -2,6 +2,25 @@ package DSA.Basic.Array;
 
 import java.util.Scanner;
 public class Search{
-
-
+public static void main(String[] args) {
+    int[] arr = new int[10];
+    int n, i, key;
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    
+    for (i = 0; i < n; i++)
+        arr[i] = sc.nextInt();
+    key = sc.nextInt();
+    boolean b1 = false;
+    for(i=0;i<n;i++){
+        if(key==arr[i]){
+         b1 = true;   
+        }
+    }
+    if(b1){
+          System.out.println("Key found");
+    }else{
+          System.out.println("Key not found");
+    }
+}
 }
