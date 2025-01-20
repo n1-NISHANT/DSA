ce3bc7598c9a420370ae1a7f66e4a0a76337637c
n1-NISHANT <nk98nishant@gmail.com>
2025-01-20 21:51:44 +0530

    Update Sum2D.java

--- Basic/Array/Sum2D.java
+++ Basic/Array/Sum2D.java
@@ -2,6 +2,21 @@ package DSA.Basic.Array;
 
 import java.util.Scanner;
 public class Sum2D{
-
-
+public static void main(String[] args) {
+    int n, i, j;
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    int arr[][] = new int[n][n];
+    
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            arr[i][j] = sc.nextInt();
+    int sum = 0;
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            sum += arr[i][j];
+        }
+    }    
+    System.out.println(sum);
+}
 }
