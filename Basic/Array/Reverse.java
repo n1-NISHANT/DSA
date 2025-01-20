fbc86ae188237997f3bde8e823e5cc73f9f9a8ef
n1-NISHANT <nk98nishant@gmail.com>
2025-01-20 21:48:59 +0530

    Update Reverse.java

--- Basic/Array/Reverse.java
+++ Basic/Array/Reverse.java
@@ -2,6 +2,15 @@ package DSA.Basic.Array;
 
 import java.util.Scanner;
 public class Reverse{
-
-
+public static void main(String[] args) {
+    int[] arr = new int[10];
+    int n, i;
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    for (i = 0; i < n; i++)
+        arr[i] = sc.nextInt();
+    for(i=1;i<=n;i++){
+        System.out.print(arr[n-i]); 
+    }
+}
 }
