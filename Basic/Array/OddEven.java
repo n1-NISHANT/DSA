07c769259e2e466c77a699692bfe5a02658dfde7
n1-NISHANT <nk98nishant@gmail.com>
2025-01-20 21:43:01 +0530

    Update OddEven.java

--- Basic/Array/OddEven.java
+++ Basic/Array/OddEven.java
@@ -2,6 +2,23 @@ package DSA.Basic.Array;
 
 import java.util.Scanner;
 public class OddEven{
-
-
+public static void main(String[] args) {
+    int[] arr = new int[10];
+    int size, i;
+    Scanner sc = new Scanner(System.in);
+    size = sc.nextInt();
+    
+    for(i = 0; i < size; i++)
+        arr[i] = sc.nextInt();
+    int Evencount =0 , Oddcount = 0;
+    for(i = 0 ;i<size;i++){
+     if(arr[i]%2==0){
+         Evencount ++;
+     }else{
+         Oddcount ++;
+     }   
+    } 
+    System.out.println("Even Number Count ="+Evencount);
+    System.out.println("Odd Number Count ="+Oddcount);
+}
 }
