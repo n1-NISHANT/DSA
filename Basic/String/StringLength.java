f6adc5bb9ed3a3f133bd56fa1808d9f848a5d3f3
n1-NISHANT <nk98nishant@gmail.com>
2025-01-21 22:13:45 +0530

    Update StringLength.java

--- Basic/String/StringLength.java
+++ Basic/String/StringLength.java
@@ -2,6 +2,13 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class StringLength{
-
+public static void main(String[] args) {
+    String temp;
+    Scanner sc = new Scanner(System.in);
+    temp = sc.nextLine();
+    
+    char[] str = temp.toCharArray();
+     System.out.println(str.length);
+}
 }
   
