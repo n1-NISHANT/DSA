ec8e2ea2e8ccc6f1f4b6c95e6e92f7cf3eedb24c
n1-NISHANT <nk98nishant@gmail.com>
2025-01-21 22:17:19 +0530

    Update CountVowels.java

--- Basic/String/CountVowels.java
+++ Basic/String/CountVowels.java
@@ -2,6 +2,27 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class CountVowels{
-
+public static void main(String[] args) {
+    String temp;
+    Scanner sc = new Scanner(System.in);
+    temp = sc.next();
+    char[] str = temp.toCharArray();
+    int count = 0;
+    for(int i =0;i<str.length;i++){
+        if(str[i]=='A'||
+           str[i]=='a'||
+           str[i]=='E'||
+           str[i]=='e'||
+           str[i]=='I'||
+           str[i]=='i'||
+           str[i]=='O'||
+           str[i]=='o'||
+           str[i]=='U'||
+           str[i]=='u'){
+            count ++;
+        }
+    }
+    System.out.println(count);
+}
 
 }
