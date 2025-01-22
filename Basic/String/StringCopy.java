222fb5910f87e4d868c9aea93853d78b75f32701
n1-NISHANT <nk98nishant@gmail.com>
2025-01-22 20:50:25 +0530

    Update StringCopy.java

--- Basic/String/StringCopy.java
+++ Basic/String/StringCopy.java
@@ -2,6 +2,16 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class StringCopy{
-
-
+public static void main(String[] args) {
+    String temp;
+    Scanner sc = new Scanner(System.in);
+    temp = sc.nextLine();
+    char[] str = temp.toCharArray();
+    String copy = "";
+    
+    for(int i =0;i<str.length;i++){
+        copy += str[i];
+    }
+    System.out.println("Copied string = "+copy);
+}
 }
