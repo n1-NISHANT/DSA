3b3b34e30ce8b14d56934cd059a1cd4df50ccf65
n1-NISHANT <nk98nishant@gmail.com>
2025-01-22 21:01:27 +0530

    Update Palindrome.java
    checking string is palindrome or not. ex- madam is a palindrome string

--- Basic/String/Palindrome.java
+++ Basic/String/Palindrome.java
@@ -2,6 +2,34 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class Palindrome{
-
-
+public static void main(String[] args) {
+    String t;
+    Scanner sc = new Scanner(System.in);
+    t = sc.nextLine();
+    char[] str = t.toCharArray();
+    char [] copy = new char[str.length];
+    for(int i = 0;i<str.length;i++){
+        copy[i]=str[i];
+    }
+    char temp = ' ';
+    int len = str.length-1;
+    for(int i = 0;i<str.length/2;i++){
+        temp = str[i];
+        str[i] = str[len-i];
+        str[len-i]= temp;
+    }
+    boolean check = false;
+    for(int i = 0; i<str.length;i++){
+        if(copy[i]==str[i]){
+            check = true;
+        }else{
+            check = false;
+        }
+    }
+    if(check){
+        System.out.println("Palindrome");
+    }else{
+        System.out.println("Not Palindrome");
+    }
+}
 }
