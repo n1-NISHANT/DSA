b97e67712366762e030f1af4e1801bb2568b47e2
n1-NISHANT <nk98nishant@gmail.com>
2025-01-22 20:56:50 +0530

    Update ReverseString1.java

--- Basic/String/ReverseString1.java
+++ Basic/String/ReverseString1.java
@@ -1,7 +1,14 @@
 package DSA.Basic.String;
 
 import java.util.Scanner;
-public class Reverse{
-
-
+public class ReverseString1{
+public static void main(String[] args) {
+    String temp;
+    Scanner sc = new Scanner(System.in);
+    temp = sc.nextLine();
+    char[] str = temp.toCharArray();
+    for(int i = str.length-1;i>=0;i--){
+        System.out.print(str[i]);
+    }
+}
 }
