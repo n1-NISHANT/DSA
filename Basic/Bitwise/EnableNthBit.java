26828f17b9dc95ff4114d8ba66ce1d84a0c963c4
n1-NISHANT <nk98nishant@gmail.com>
2025-01-24 19:44:44 +0530

    Update EnableNthBit.java

--- Basic/Bitwise/EnableNthBit.java
+++ Basic/Bitwise/EnableNthBit.java
@@ -2,6 +2,11 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class EnableNthBit{
-
-
+public static void main(String[] args) {
+    int num, n;
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    n = sc.nextInt();
+    System.out.println(num|(1<<n-1));
+}
 }
