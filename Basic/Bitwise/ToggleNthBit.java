4fe5efcb8cb50c0dd823014981614adc2279596b
n1-NISHANT <nk98nishant@gmail.com>
2025-01-24 19:47:39 +0530

    Update ToggleNthBit.java

--- Basic/Bitwise/ToggleNthBit.java
+++ Basic/Bitwise/ToggleNthBit.java
@@ -2,6 +2,11 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class ToggleNthBit{
-
-
+public static void main(String[] args) {
+    int num, n;
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    n = sc.nextInt();
+    System.out.println(num^(1<<n-1));
+}
 }
