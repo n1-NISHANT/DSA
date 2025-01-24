603dda6176b287acda2bd42defffd4344f571fb2
n1-NISHANT <nk98nishant@gmail.com>
2025-01-24 19:41:57 +0530

    Update FindEqualOrNot.java

--- Basic/Bitwise/FindEqualOrNot.java
+++ Basic/Bitwise/FindEqualOrNot.java
@@ -2,6 +2,16 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class FindEqualOrNot{
-
-
+public static void main(String[] args) {
+    int num1, num2;
+    
+    Scanner sc = new Scanner(System.in);
+    num1 = sc.nextInt();
+    num2 = sc.nextInt();
+    if((num1 ^ num2)==0){
+        System.out.println("Equal");
+    }else{
+        System.out.println("Unequal");
+    }
+}
 }
