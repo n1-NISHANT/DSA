2752db1a952189b2fc781ff94e40ec2b1c032a16
n1-NISHANT <nk98nishant@gmail.com>
2025-01-24 19:42:51 +0530

    Update OddEven.java

--- Basic/Bitwise/OddEven.java
+++ Basic/Bitwise/OddEven.java
@@ -2,6 +2,14 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class oddEven{
-
-
+public static void main(String[] args) {
+    int num;
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    if((num&1)==0){
+        System.out.println("Even");
+    }else{
+        System.out.println("Odd");
+    }
+}
 }
