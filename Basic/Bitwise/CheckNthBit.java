5aec68d3d78896cb7e992e94d72dd012a8fe5a83
n1-NISHANT <nk98nishant@gmail.com>
2025-01-24 19:45:52 +0530

    Update CheckNthBit.java

--- Basic/Bitwise/CheckNthBit.java
+++ Basic/Bitwise/CheckNthBit.java
@@ -2,6 +2,15 @@ package DSA.Basic.String;
 
 import java.util.Scanner;
 public class CheckNthBit{
-
-
+public static void main(String[] args) {
+    int num, n;
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    n = sc.nextInt();
+    if((num & (1<<n-1))== 0){
+        System.out.println("OFF");
+    }else{
+        System.out.println("ON");
+    }
+}
 }
