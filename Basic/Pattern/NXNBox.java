5e32b9212a08e626489452f0e11c3800f3e6aeca
n1-NISHANT <nk98nishant@gmail.com>
2025-01-26 22:51:33 +0530

    Update NXNBox.java

--- Basic/Pattern/NXNBox.java
+++ Basic/Pattern/NXNBox.java
@@ -2,5 +2,15 @@ package DSA.Basic.Pattern;
 
 import java.util.Scanner;
 public class NXNBox{
-
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int i, j;
+   for(i = 1; i<=n;i++){
+       for(j=1;j<=n;j++){
+           System.out.print("* ");
+       }
+       System.out.println();
+   }
+}
 }
