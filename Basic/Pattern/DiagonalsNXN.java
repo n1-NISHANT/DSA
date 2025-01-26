6ec061d9b4cd41dcd94135e6188083b92abc1d2f
n1-NISHANT <nk98nishant@gmail.com>
2025-01-26 22:53:23 +0530

    Update DiagonalsNXN.java

--- Basic/Pattern/DiagonalsNXN.java
+++ Basic/Pattern/DiagonalsNXN.java
@@ -2,6 +2,19 @@ package DSA.Basic.Pattern;
 
 import java.util.Scanner;
 public class DiagonalsNXN{
-
-
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int i, j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(i==j || i+j==n+1){
+                System.out.print("* ");
+            }else{
+                System.out.print("  ");
+            }
+        }
+        System.out.println();
+    }
+}
 }
