1b8a1ac59ee2aa45286d61e97f578aa216926cb8
n1-NISHANT <nk98nishant@gmail.com>
2025-01-26 22:52:28 +0530

    Update HollowNXN.java

--- Basic/Pattern/HollowNXN.java
+++ Basic/Pattern/HollowNXN.java
@@ -2,5 +2,19 @@ package DSA.Basic.Pattern;
 
 import java.util.Scanner;
 public class HollowNXN{
-
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int i, j;
+    for(i = 1; i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(i==1 || i==n || j==1 || j==n){
+                System.out.print("* ");
+            }else{
+                System.out.print("  ");
+            }
+        }
+        System.out.println();
+    }
+}
 }
