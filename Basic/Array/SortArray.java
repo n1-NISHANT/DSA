ff6e66580e08d4395d072e32bc62b18e2ddbf3c9
n1-NISHANT <nk98nishant@gmail.com>
2025-01-27 20:27:18 +0530

    Update SortArray.java

--- Basic/Array/SortArray.java
+++ Basic/Array/SortArray.java
@@ -1,1 +1,21 @@
+package DSA.Basic.Array;
 
+import java.util.Scanner;
+public class SortArray{
+  
+
+
+  
+   public static void main(String [] args){
+      Scanner sc = new Scanner(System.in); 
+      System.out.print("Enter length of array : ");
+      int value = sc.nextInt();
+      int arr[] = new int[value];
+      for(int i = 0;i<value;i++){
+        arr[i] = sc.nextInt();
+      }
+      System.out.println("Before Sorting : ");
+      for(int i : arr){
+        System.out.print(arr[i]+"\t");
+   }
+}
