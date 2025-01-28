8fb9812d3e818a34933ba0a986bc0d00dec6a0f0
n1-NISHANT <nk98nishant@gmail.com>
2025-01-28 20:51:51 +0530

    Update SortArray.java

--- Basic/Array/SortArray.java
+++ Basic/Array/SortArray.java
@@ -2,11 +2,7 @@ package DSA.Basic.Array;
 
 import java.util.Scanner;
 public class SortArray{
-  
-
-
-  
-   public static void main(String [] args){
+   public int[] input(){
       Scanner sc = new Scanner(System.in); 
       System.out.print("Enter length of array : ");
       int value = sc.nextInt();
@@ -14,8 +10,25 @@ public class SortArray{
       for(int i = 0;i<value;i++){
         arr[i] = sc.nextInt();
       }
-      System.out.println("Before Sorting : ");
-      for(int i : arr){
-        System.out.print(arr[i]+"\t");
+       sc.close();
+     return arr;
    }
+
+  public int[] sortFunction( int arr[]){
+        
+
+    return arr;
+  }
+   public static void main(String [] args){
+       SortArray sortArrey = new SortArray();
+       int arr[] = sortArray.input();
+            System.out.println("Before Sorting : ");
+            for(int i : arr){
+              System.out.print(arr[i]+"\t");
+             }
+        int arr1[] = sortArray.sortFunction(arr);
+           System.out.println("After Sorting : ");
+            for(int i : arr2){
+              System.out.print(arr[i]+"\t");
+             }
 }
