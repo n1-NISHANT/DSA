73aefb9b5b969f807f300d4ed5a7dcc40e4d2725
n1-NISHANT <nk98nishant@gmail.com>
2025-01-29 20:50:54 +0530

    Update PrintArrayWithoutLoop.java
    Printing Array Without Loop Using Recursion.

--- Basic/Array/PrintArrayWithoutLoop.java
+++ Basic/Array/PrintArrayWithoutLoop.java
@@ -1,1 +1,30 @@
+package DSA.Basic.Array;
 
+class Solution {
+    static int x ;
+    static int arr [];
+    
+       public static int[] printNos(int x) {
+	        Solution.x=x;
+	        arr= new int[x];
+	        new Solution().recursion();
+	        return arr;
+		    }
+		     int i = 0;
+		     int j = 1;
+		     void recursion(){
+		        arr[i] = j;
+		        ++i;
+		        ++j;
+		        if(i<x)
+		            recursion();
+    } 
+}
+public class PrintArrayWithoutLoop {
+	public static void main(String[] args) {
+	int arr []= Solution.printNos(5);
+      	for(int i = 0; i<arr.length;i++) {
+      		System.out.println(arr[i]);
+        }
+    }
+}
