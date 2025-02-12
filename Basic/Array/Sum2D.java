6d28db20f629b5ef45aed4c63ad0767b2d16c24d
n1-NISHANT <nk98nishant@gmail.com>
2025-02-12 21:06:34 +0530

    Update Sum2D.java

--- Basic/Array/Sum2D.java
+++ Basic/Array/Sum2D.java
@@ -7,7 +7,6 @@ public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     n = sc.nextInt();
     int arr[][] = new int[n][n];
-    
     for(i = 0; i < n; i++)
         for(j = 0; j < n; j++)
             arr[i][j] = sc.nextInt();
